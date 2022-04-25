46c6c3a8d5da03a471f456bc27609d9457373207
Cphayim <i@cphayim.me>
2022-04-26 00:47:02 +0800

    chore: 初始化开发和测试环境

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  root: __dirname,
+  resolve: {
+    alias: [
+      {
+        find: /^@cy-pg\/vue-(.*)$/,
+        replacement: resolve(__dirname, 'packages/$1/src'),
+      },
+    ],
+  },
+  test: {
+    environment: 'node',
+    include: ['packages/**/__tests__/**/*.ts'],
+    coverage: {
+      include: ['packages/**/src/**/*.ts'],
+      reporter: ['text', 'lcov'],
+      statements: 90,
+    },
+  },
+})
